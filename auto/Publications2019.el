0f095195fdf3bdc088be8ad09c8b2e3ed959410a
Konstantin Genin <konstantin.genin@gmail.com>
2019-03-08 21:28:43 -0800

    Update Talk

---
+++ auto/Publications2019.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "Publications2019"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("res" "12pt")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "res"
+    "res12"
+    "helvet"
+    "hyperref"
+    "marvosym"))
+ :latex)
+
